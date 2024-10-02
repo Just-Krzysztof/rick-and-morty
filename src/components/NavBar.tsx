e43f738110725f1751f8641ffa0272cbd93f97db
Krzysztof <krzysztof@dawidrza.com>
2024-10-02 17:35:28 +0200

    color switcher

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import Logo from "../assets/logo.png";
-import { Image } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
+import ColorModeSwitch from "./ColorModeSwitcher";
 const NavBar = () => {
-  return <Image src={Logo} width={{ sm: "60%", md: "30%", lg: "20%" }} />;
+  return (
+    <HStack justifyContent='space-between' padding='10px'>
+      <Image src={Logo} width={{ sm: "60%", md: "30%", lg: "20%" }} />;
+      <ColorModeSwitch />
+    </HStack>
+  )
+  
 };
 
 export default NavBar;
