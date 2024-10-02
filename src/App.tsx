bbcc913cd6932e61540ec038d5b8e1a84910f7c7
Krzysztof <krzysztof@dawidrza.com>
2024-10-02 11:50:45 +0200

    Little fix with Grid and card

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
-import { useState } from 'react'
-import CharacterGrid from './components/CharacterGrid'
+import { useState } from "react";
+import CharacterGrid from "./components/CharacterGrid";
+import { Box, Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-
   return (
     <>
-      <CharacterGrid />
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+        templateColumns={{
+          base: "1fr",
+          lg: "200px 1fr",
+        }}
+      >
+        <GridItem area="nav" bg="coral">
+          nav
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside">aside</GridItem>
+        </Show>
+        <GridItem area="main" bg="dodgerblue">
+          <CharacterGrid />
+        </GridItem>
+      </Grid>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/CharacterGrid.tsx
+++ src/components/CharacterGrid.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import useCharacters from "../hooks/useCharacters";
 import CharacterCard from "./CharacterCard";
 import { SimpleGrid } from "@chakra-ui/react";
+import CharacterCardContainer from "./CharacterCardContainer";
 
 const CharacterGrid = () => {
   const { data, error, isLoading } = useCharacters();
@@ -10,9 +11,11 @@ const CharacterGrid = () => {
   if (error) return <p>Error: {error}</p>;
 
   return (
-    <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xlg:4 }} spacing={10}>
+    <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={10}>
       {data.map((character) => (
-        <CharacterCard key={character.id} character={character} />
+        <CharacterCardContainer>
+          <CharacterCard key={character.id} character={character} />
+        </CharacterCardContainer>
       ))}
     </SimpleGrid>
   );
